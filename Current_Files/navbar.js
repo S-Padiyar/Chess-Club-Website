3d9519e4d3b9839064c4fd01294d35ac0d4ae553
Sunmay Padiyar <sunmayp@gmail.com>
2025-07-04 02:53:03 -0400

    Finished contact page

--- Current_Files/navbar.js
+++ Current_Files/navbar.js
@@ -21,3 +21,12 @@ Array.from(navLinks).forEach(link => {
   });
 });
 
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
